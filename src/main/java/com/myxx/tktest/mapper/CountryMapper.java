9f61a35024fad7197d3b19143bb5aaac265b7167
qqchen <785019257@qq.com>
2022-03-24 15:06:54 +0800

    Oracle数据库代码自动生成配置修改

--- src/main/java/com/myxx/tktest/mapper/CountryMapper.java
+++
@@ -1,7 +0,0 @@
-package com.myxx.tktest.mapper;
-
-import com.myxx.tktest.pojo.Country;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CountryMapper extends Mapper<Country> {
-}
--- src/main/java/com/myxx/tktest/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.myxx.tktest.mapper;
-
-import com.myxx.tktest.pojo.User;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-}
