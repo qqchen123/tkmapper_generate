ea5987eaa2eb2cf19db9bd09a1c169a6f24d824f
qqchen <785019257@qq.com>
2022-03-24 17:10:28 +0800

    添加日志框架

--- src/main/java/com/myxx/tktest/controller/ProjectController.java
+++ src/main/java/com/myxx/tktest/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.myxx.tktest.controller;
 
 import com.myxx.tktest.pojo.ProjectInfo;
 import com.myxx.tktest.service.ProjectInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/project")
 public class ProjectController {
