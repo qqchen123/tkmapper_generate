d115252660642b482a02dd6dc111ef4a61fc4ac4
ejchen <785019257@qq.com>
2022-07-08 21:19:14 +0800

    用本地搭建的tomcat服务器

--- src/main/java/com/myxx/tktest/TktestApplication.java
+++ src/main/java/com/myxx/tktest/TktestApplication.java
@@ -2,11 +2,17 @@ package com.myxx.tktest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@tk.mybatis.spring.annotation.MapperScan(basePackages = "com.myxx.tktest.mapper")
 @SpringBootApplication
-public class TktestApplication {
+public class TktestApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TktestApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(TktestApplication.class, args);
     }
--- src/main/java/com/myxx/tktest/utils/WebMvcConfig.java
+++ src/main/java/com/myxx/tktest/utils/WebMvcConfig.java
@@ -9,7 +9,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+//                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true);
